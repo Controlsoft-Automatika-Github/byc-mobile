95736281f24c0d0867dfe52ba75c1b48d2450dc9
Sörös Ádám <57035872+AdamBeer2000@users.noreply.github.com>
2024-03-05 09:57:48 +0100

    sw 2

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/byc-mobile/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/byc-mobile/sw.js', { scope: '/byc-mobile/' })})}
